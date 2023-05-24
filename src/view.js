4c2b4a6f2789082417c86ae1a054c5f30e97bb11
Svetlana Khodak <yasminaestel@gmail.com>
2023-05-24 19:17:28 +0300

    conclusion items

--- src/view.js
+++ src/view.js
@@ -58,11 +58,57 @@ const renderFeeds = (feeds) => {
   });
 };
 
+const renderItems = (items) => {
+  const containerItems = document.querySelector('.posts');
+  containerItems.innerHTML = '';
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+  containerItems.append(card);
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  card.append(cardBody);
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardBody.append(cardTitle);
+  cardTitle.textContent = 'Посты';
+  console.log(items);
+
+  items.forEach((array) => {
+    array.forEach((item) => {
+      const listGroup = document.createElement('ul');
+      listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+      card.append(listGroup);
+      const listGroupItem = document.createElement('li');
+      listGroupItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+      listGroup.append(listGroupItem);
+      const link = document.createElement('a');
+      link.setAttribute('href', item.link);
+      link.classList.add('fw-bold');
+      link.setAttribute('data-id', item.parentsFeed);
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
+      listGroupItem.append(link);
+      link.textContent = item.link;
+      const button = document.createElement('button');
+      button.setAttribute('type', 'button');
+      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+      button.setAttribute('data-id', item.parentsFeed);
+      button.setAttribute('data-bs-toggle', 'modal');
+      button.setAttribute('data-bs-target', '#modal');
+      listGroupItem.append(button);
+      button.textContent = 'Просмотр';
+    });
+  });
+};
+
 const render = (path, i18next, state) => {
   if (path === 'error') {
     renderForm(i18next, state);
   } if (path === 'feeds') {
     renderFeeds(state.feeds);
+  } if (path === 'items') {
+    renderItems(state.items);
   }
 };
 
