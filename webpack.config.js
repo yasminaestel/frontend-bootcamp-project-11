13c1716792dc952ce8aa7eb81074f26de5277c77
Svetlana Khodak <yasminaestel@gmail.com>
2023-05-29 21:29:45 +0300

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,7 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    open: true,
-    host: 'localhost',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
@@ -49,5 +29,12 @@ export default {
       },
     ],
   },
-  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  output: {
+    clean: true,
+  },
 };
